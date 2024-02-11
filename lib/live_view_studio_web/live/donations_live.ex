a1f96d0f32218444bb6eeaac732bf54299e8db15
Hashem Sami <hashem.sf@gmail.com>
2024-02-11 21:15:27 +0300

    working on sorting

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,42 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
+    # we allow other params to be passed, but we will
+    # defaulting our params if not found
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    donations = Donations.list_donations(options)
+
+    {:noreply, assign(socket, donations: donations, options: options)}
+  end
+
+  attr :options, :map, required: true
+  attr :sort_by, :string
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
   end
 end

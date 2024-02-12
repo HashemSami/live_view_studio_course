6c0a6978a1888a478a8a200665cd2c175a81f1e9
Hashem Sami <hashem.sf@gmail.com>
2024-02-12 21:09:12 +0300

    working on paginations

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -10,12 +10,17 @@ defmodule LiveViewStudioWeb.DonationsLive do
   def handle_params(params, _uri, socket) do
     # we allow other params to be passed, but we will
     # defaulting our params if not found
-    sort_by = (params["sort_by"] || "id") |> String.to_atom()
-    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+    sort_by = valid_sort_by(params)
+    sort_order = valid_sort_order(params)
+
+    page = (params["page"] || "1") |> String.to_integer()
+    per_page = (params["per_page"] || "5") |> String.to_integer()
 
     options = %{
       sort_by: sort_by,
-      sort_order: sort_order
+      sort_order: sort_order,
+      page: page,
+      per_page: per_page
     }
 
     donations = Donations.list_donations(options)
@@ -23,15 +28,32 @@ defmodule LiveViewStudioWeb.DonationsLive do
     {:noreply, assign(socket, donations: donations, options: options)}
   end
 
+  def handle_event("page_select", %{"per-page" => per_page}, socket) do
+    params = %{socket.assigns.options | per_page: per_page}
+
+    # this will invoke the handle params function to handle the URL
+    # params and render the new state from there
+    # in this case we are initiating the path from the server side
+    # instead of doing it on the client
+    socket = push_patch(socket, to: ~p"/donations?#{params}")
+    {:noreply, socket}
+  end
+
   attr :options, :map, required: true
   attr :sort_by, :atom, required: true
   slot :inner_block, required: true
 
   def sort_link(assigns) do
+    params = %{
+      assigns.options
+      | sort_by: assigns.sort_by,
+        sort_order: next_sort_order(assigns.options.sort_order)
+    }
+
+    assigns = assign(assigns, params: params)
+
     ~H"""
-    <.link patch={
-      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
-    }>
+    <.link patch={~p"/donations?#{@params}"}>
       <%= render_slot(@inner_block) %> <%= sort_indicator(
         @sort_by,
         @options
@@ -56,4 +78,18 @@ defmodule LiveViewStudioWeb.DonationsLive do
   end
 
   defp sort_indicator(_, _), do: ""
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(item quantity days_until_expires) do
+    String.to_existing_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order})
+       when sort_order in ~w(asc desc) do
+    String.to_existing_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
